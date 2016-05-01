1122ff12967f62010c67b134e490c42822170a47
sbibke101 <sbibek101@hotmail.com>
2016-04-30 21:14:51 -0700

    Console app solns file added

---
+++ LFAProjects/Shared project/LFA Data/LFA.Data/User.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFA.Data
+{
+    public class User
+    {
+        #region public member
+        public string UserName { get; set; }
+       
+
+        public int UserID { get; set; }
+        #endregion
+       
+        
+    # region constructor
+        public User()
+        {
+            UserName = "guest";
+            Password = "admin";
+           
+        }
+
+        /// <summary>
+        /// Create user with user name and Password
+        /// </summary>
+        /// <param name="UserName"></param>
+        /// <param name="Password"></param>
+        public User(string userName,string passWord)
+
+        {
+            UserName = userName;
+            Password = passWord;
+
+            
+        }
+    #endregion 
+
+        #region private member
+        public string Password { get; set; }
+#endregion 
+
+
+        #region public method
+
+        public void authenticat()
+        {
+            
+            if((UserName=="admin")&&(Password=="admin"))
+            {
+                Console.WriteLine("you are administrator",UserName);
+            }
+            else if((UserName == "User") && (Password == "Pass"))
+            {
+                Console.WriteLine("you are normal user");
+
+            }
+            else
+            {
+                Console.WriteLine("you are  in valid user");
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ LFAProjects/UI/LFAConoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using LFA.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to leapfrog");
+
+            Console.Write("Enter your name");
+            string strname = Console.ReadLine();
+
+            Console.Write("Enter your password");
+            string strpass = Console.ReadLine();
+
+
+
+
+            int count = 1;
+            User objUser = new User();
+            objUser.UserID = 1;
+            objUser.UserName = "strname";
+            objUser.Password = "strpass";
+            objUser.authenticat();
+
+       //     Console.WriteLine("User name of obj user is :" + objUser.UserName);
+         //   Console.WriteLine("password is {0}", objUser.Password);
+        //    Console.WriteLine("your count is " + count);
+          //  var objUsercopy = objUser;
+           // objUsercopy.UserName = "NewUser";
+           // Console.WriteLine("New user name is " + objUsercopy.UserName);
+
+//            int countcopy = count;
+  //          countcopy++;
+    //        Console.WriteLine("your count is " + count);
+      //      Console.WriteLine("your count is " + countcopy++);
+
+
+
+
+
+           
+
+            //    loginUser();
+            //  Console.WriteLine("press any key to exit");
+            //Console.ReadLine();
+
+
+            //    Console.Write("Enter your name :");
+
+            //   string strname = Console.ReadLine();
+            // Console.Write("Enter your password :");
+
+            //string strpass = Console.ReadLine();
+            //if  ((strname == "admin") && (strpass == "admin")) 
+            //    {
+            //   Console.Write("you are authenticated  welcome " + strname);
+
+            //                }
+            //          else
+            //          {
+            //          Console.Write("Enter the valid user name and password");
+            //       }
+
+            Console.ReadKey();
+        }
+    }
+}
